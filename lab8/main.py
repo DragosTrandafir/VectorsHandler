105634b6b709354aa190a145ffdb69c9f2976fb4
Dragos Trandafir <62999548+DragosTrandafir@users.noreply.github.com>
2023-12-20 17:58:17 +0200

    Delete lab8 directory

--- lab8/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.append(r"D:\seminar8")
-from logic.vector_repo import vectorRepoInitial
-from console.ui import VectorUI
-
-
-vR=vectorRepoInitial
-user=VectorUI(vR)
-user.start()
